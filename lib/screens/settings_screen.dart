3324f70c8d0605ae56e68fde5adddc15a503b7f5
Hatsakon <hasakron@gmail.com>
2023-01-17 22:53:21 +0700

    first commit

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,106 @@
+import '../widgets/main_drawer.dart';
+
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  final Function saveSetting;
+  final Map<String, bool> currentSettings;
+
+  const SettingsScreen(this.saveSetting, this.currentSettings);
+
+  static const routeName = '/settings';
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  @override
+  initState() {
+    _glutenFree = widget.currentSettings['gluten']!;
+    _lactoseFree = widget.currentSettings['lactose']!;
+    _vegetarian = widget.currentSettings['vegetarian']!;
+    _vegan = widget.currentSettings['vegan']!;
+    super.initState();
+  }
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function(bool) updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Settings'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final selectedSettings = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              widget.saveSetting(selectedSettings);
+            },
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
+      drawer: MainDrawer(),
+      body: Column(children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            'Adjust your meal selection.',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        Expanded(
+          child: ListView(
+            children: [
+              _buildSwitchListTile(
+                  'Gluten-Free', 'Only include gluten-free meals.', _glutenFree,
+                  (newValue) {
+                setState(() {
+                  _glutenFree = newValue;
+                });
+              }),
+              _buildSwitchListTile('Lactose-Free',
+                  'Only include lactose-free meals.', _lactoseFree, (newValue) {
+                setState(() {
+                  _lactoseFree = newValue;
+                });
+              }),
+              _buildSwitchListTile(
+                  'Vegetarian', 'Only include vegetarian meals.', _vegetarian,
+                  (newValue) {
+                setState(() {
+                  _vegetarian = newValue;
+                });
+              }),
+              _buildSwitchListTile('Vegan', 'Only include vegan meals.', _vegan,
+                  (newValue) {
+                setState(() {
+                  _vegan = newValue;
+                });
+              }),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,72 @@
+import '../widgets/main_drawer.dart';
+
+import './favorites_screen.dart';
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+import '../models/meal.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  const TabsScreen(this.favoriteMeals);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  late List<Map<String, dynamic>> _pages;
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': FavoritesScreen(widget.favoriteMeals),
+        'title': 'Your Favorites',
+      },
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title']),
+      ),
+      drawer: Drawer(
+        child: MainDrawer(),
+      ),
+      body: _pages[_selectPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
